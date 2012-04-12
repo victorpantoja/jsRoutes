5ba14a970c7d9aad1bea8aeb90c778278c830405
Victor Pantoja <victor.pantoja@gmail.com>
2012-04-12 18:44:04 -0300

    jasmine tests

--- tests/jasmine/spec/jsRouteSpec.js
+++ tests/jasmine/spec/jsRouteSpec.js
@@ -1,22 +1,33 @@
 describe("jsRoute", function() {
 
-  beforeEach(function() {
+	beforeEach(function() {
+		//route.routes = [];
+		//route.funcs = [];
+	});
 
-  });
+	it("should be able to add routes", function() {
+		route.add('^$', function(){self.x = self.x+1;});
+		expect(route.routes.indexOf("^$") > -1).toBeTruthy();
+	});
 
-  it("should be able to add routes", function() {
-
-  });
-  
-  it("should be able to init", function() {
-
-  });
-  
-  it("should be able to check for updates", function() {
-
-  });
-  
-  it("should be able to update", function() {
-
-  });
+	it("should be able to init routing", function() {
+		location.hash="#22-10-2012";
+		
+		route.add('^\\d{1,2}\\-\\d{1,2}\-\\d{4}$', function(x){
+			if(x==='22-10-2012'){
+				location.hash='#alterado';
+			}
+		});
+		
+		runs(function(){
+			route.init();
+		});
+		
+		waits(1000);
+		
+		runs(function(){
+			expect(location.hash).toEqual('#alterado');
+		});
+		
+	});
 });
