31780847867f8005f63d6b866bb98f1bdad87ef6
Victor Pantoja <victor.pantoja@gmail.com>
2012-04-12 17:41:41 -0300

    jasmine unit tests

---
+++ tests/jasmine/spec/jsRouteSpec.js
@@ -0,0 +1,22 @@
+describe("jsRoute", function() {
+
+  beforeEach(function() {
+
+  });
+
+  it("should be able to add routes", function() {
+
+  });
+  
+  it("should be able to init", function() {
+
+  });
+  
+  it("should be able to check for updates", function() {
+
+  });
+  
+  it("should be able to update", function() {
+
+  });
+});
